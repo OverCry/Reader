f9b2960df01d4bdb8348dc35ad8e4d1db2287bba
Wong <wcho400@aucklanduni.ac.nz>
2024-10-19 19:44:49 +1300

    Update with aliases

--- craco.config.ts
+++ craco.config.ts
@@ -3,7 +3,11 @@ import path from 'path';
 export default {
   webpack: {
     alias: {
+      '@MainContext': path.resolve(__dirname, 'src/AppContext/Provider/DirectoryContext'),
+      '@LocalStorage': path.resolve('src/AppContext/LocalStorage/localStorage'),
+      '@Colours': path.resolve('src/Theme/colours'),
       '@GenericFunctions': path.resolve(__dirname, 'src/components/Support/genericFunctions'),
+      '@Headers': path.resolve(__dirname, 'src/components/Support/headers'),
     },
     configure: (webpackConfig: unknown) => {
       // Custom Webpack changes can be made here
